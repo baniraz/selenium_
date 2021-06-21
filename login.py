0417959a303b17f743ea1b16b2e8a194f605e8f0
baniraz <51762781+baniraz@users.noreply.github.com>
2021-06-21 12:42:26 +0300

    Update login.py
    Задание 7. Сделайте сценарий, проходящий по всем разделм админки

--- login.py
+++ login.py
@@ -16,7 +16,13 @@ def test_login(driver):
     driver.find_element_by_name("password").send_keys("admin")
     driver.find_element_by_name("remember_me").click()
     driver.find_element_by_name("login").click()
-    driver.find_element_by_link_text('Appearence').click()
-    time.sleep(2)
+    # driver.find_element_by_link_text('Appearence').click()
+    # time.sleep(2)
+    for section in range(1, len(driver.find_elements_by_css_selector("#app-")) + 1):
+        driver.find_element_by_css_selector(f"#app-:nth-child({section})").click()
+        if driver.find_elements_by_css_selector(f"#app-:nth-child({section}) li") is not None:
+            for sub_section in range(1, len(driver.find_elements_by_css_selector(f"#app-:nth-child({section}) li")) + 1):
+                driver.find_element_by_css_selector(f"#app-:nth-child({section}) li:nth-child({sub_section})").click()
+                assert driver.find_element_by_css_selector("#content > h1"), "Заголовок H1 не найден"
     driver.find_element_by_css_selector('[title="Logout"]').click()
     driver.find_element_by_css_selector('div [class="content"]')
