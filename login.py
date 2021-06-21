b38e9597cd524fa296bbb6aa1096352ac27430d3
baniraz <51762781+baniraz@users.noreply.github.com>
2021-06-21 20:15:09 +0300

    Задание 9
    Проверить сортировку стран и геозон в админке

--- login.py
+++ login.py
@@ -2,7 +2,7 @@ import pytest
 import time
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
-
+from helpers import login_admin
 
 @pytest.fixture
 def driver(request):
@@ -39,3 +39,54 @@ def test_check_stickers(driver):
             print('U ' + duck.find_element_by_xpath('../div[@class="name"]').text + ' Нет стикера!')
     for duck in ducks:
         duck.find_element_by_css_selector('div.sticker')
+
+
+def sort_checker(li):
+    li_sort = li.copy()
+    li_sort = sorted(li_sort)
+    assert li_sort == li, f"Список не отсортирован"
+
+
+def check_countries(countries_elems):
+    countries_list = []
+    countries_to_check = []
+    for i in countries_elems:
+        countries_list.append(i.find_element_by_css_selector('td:nth-child(5)').text)
+        if int(i.find_element_by_css_selector('td:nth-child(6)').text) > 0:
+            countries_to_check.append(i.find_element_by_css_selector('td:nth-child(5)').text)
+    sort_checker(countries_list)
+    return countries_to_check
+
+
+def check_zones(driver, countries_to_check, backpage, where_is_the_zone, how_to_check):
+    for country in countries_to_check:
+        driver.find_element_by_xpath(f"//a[contains(text(), '{country}')]").click()
+        zone_rows = driver.find_elements_by_css_selector('table#table-zones > tbody > tr')
+        zones = []
+        for i in zone_rows[1: -1]:
+            zone_name = i.find_element_by_xpath(where_is_the_zone)
+            zones.append(zone_name.get_attribute(how_to_check))
+        sort_checker(zones)
+        driver.get(backpage)
+
+
+def test_list_sorts_in_countries(driver):
+    login_admin(driver)
+    driver.get("http://localhost/litecart/admin/?app=countries&doc=countries/")
+    countries_url = 'http://localhost/litecart/admin/?app=countries&doc=countries/'
+    driver.get(countries_url)
+    countries_elems = driver.find_elements_by_css_selector('table.dataTable > tbody > tr.row')
+    countries_to_check = check_countries(countries_elems)
+    check_zones(driver, countries_to_check, countries_url, './td[3]/input', 'value')
+    driver.close()
+
+
+def test_list_sorts_in_zones(driver):
+    login_admin(driver)
+    zones_url = 'http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones/'
+    driver.get(zones_url)
+    countries_elems = driver.find_elements_by_css_selector('table.dataTable > tbody > tr.row')
+    countries_to_check = []
+    for i in countries_elems:
+        countries_to_check.append(i.find_element_by_css_selector('td:nth-child(3) > a').text)
+    check_zones(driver, countries_to_check, zones_url, './td[3]/select/option[@selected="selected"]', 'text')
